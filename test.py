e077595ddf0a2296c83d1fe61fc2b1bc4b9f3a65
Kush Shah <kushshah253@gmail.com>
2020-05-13 20:23:12 +0530

    practicing making commits and adding them to github

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import pandas as pd
-filename = "/BulkDoownloadListing.csv"
-df = pd.read_csv(filename, nrows = 20)
+# filename = "/BulkDoownloadListing.csv"
+# df = pd.read_csv(filename, nrows = 20)
+df = pd.DataFrame([['A', 'B', 'C'],[1,2,3]])
 print(df.head())
-
+print("Dataframe printed successfully..>!")
